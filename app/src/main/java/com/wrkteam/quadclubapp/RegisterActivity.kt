312d2737b235ea5c41438500a94d5c8ffd117312
Robert <robert10290@gmail.com>
2021-03-22 00:39:20 +0100

    RegisterActivity.kt 2

--- app/src/main/java/com/wrkteam/quadclubapp/RegisterActivity.kt
+++ app/src/main/java/com/wrkteam/quadclubapp/RegisterActivity.kt
@@ -2,10 +2,24 @@ package com.wrkteam.quadclubapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_register)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "Register"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener{ it: View1
+            val intent = Intent(packageContext: this@RegisterActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        mAuth = FirebaseAuth.getInstance()
+
     }
 }
