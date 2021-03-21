7d9002748db86a6102c165567ca10f42da57e617
Wojtek <wojciechd23@gmail.com>
2021-03-21 23:44:50 +0100

    trzeba dopisać binging

--- app/src/main/java/com/wrkteam/quadclubapp/LoginActivity.kt
+++ app/src/main/java/com/wrkteam/quadclubapp/LoginActivity.kt
@@ -2,10 +2,25 @@ package com.wrkteam.quadclubapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
+
+
+        btn_to_login.setOnClickListener {
+            loginUser();
+        }
+    }
+
+    private fun loginUser() {
+        val email: String = txt_log_email.text.toString()
     }
 }
