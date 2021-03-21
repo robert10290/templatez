715bb7769cd727a651ebf57a32a31a5a3f240093
Wojtek <wojciechd23@gmail.com>
2021-03-22 00:18:23 +0100

    próba loginu

--- app/src/main/java/com/wrkteam/quadclubapp/LoginActivity.kt
+++ app/src/main/java/com/wrkteam/quadclubapp/LoginActivity.kt
@@ -1,24 +1,54 @@
 package com.wrkteam.quadclubapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.wrkteam.quadclubapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         mAuth = FirebaseAuth.getInstance()
 
-        btn_to_login.setOnClickListener {
+        binding.btnToLogin.setOnClickListener {
             loginUser();
         }
     }
 
     private fun loginUser() {
-        val email: String = txt_log_email.text.toString()
+        val email: String = binding.txtLogEmail.text.toString()
+        val password: String = binding.txtLogPassword.text.toString()
+
+        if(email == "")
+        {
+            Toast.makeText(this@LoginActivity, "please write email", Toast.LENGTH_LONG).show()
+        }
+        else if (password == "")
+        {
+            Toast.makeText(this@LoginActivity, "please write password", Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful)
+                    {
+                        val intent = Intent(this, MainActivity::class.java).apply {}
+                        startActivity(intent)
+                    }
+                    else
+                    {
+                        Toast.makeText(this@LoginActivity, "Error Message: you don't remember yout password noob!", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
     }
 }
